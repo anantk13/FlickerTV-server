207bad22c5d1191a33eebdf51ea054b385317fce
Anantk13 <anant.kapooor@gmail.com>
2025-09-22 01:18:29 +0530

    upd 3

--- serverless.js
+++ serverless.js
@@ -1,5 +1,5 @@
 import serverless from "serverless-http";
-import { app } from "./src/app.js"; // adjust path if app.js is in src folder
+import  app  from "./src/app.js"; // adjust path if app.js is in src folder
 
 // export default handler for Vercel
 export default serverless(app);
