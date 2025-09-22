4461df73f81190704baedbd1d33336799c7e2bf6
Anantk13 <anant.kapooor@gmail.com>
2025-09-22 15:23:38 +0530

    update

--- serverless.js
+++ serverless.js
@@ -1,5 +1,4 @@
 import serverless from "serverless-http";
 import app from "./src/app.js";
 
-// export named handler for Vercel
-export const handler = serverless(app);
+export default serverless(app);
