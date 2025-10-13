1e385ce5d6d06cb307a88ec55ffc5e8957a89df8
Anantk13 <anant.kapooor@gmail.com>
2025-10-13 13:14:24 +0530

    upd

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -93,6 +93,60 @@ const getAllVideos = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, videos, "Videos fetched successfully"));
 });
 
+const publishAVideo = asyncHandler(async (req, res) => {
+  const { title, description, isPublished } = req.body;
+
+  if (
+    [title, description, isPublished].some(
+      (field) => field === undefined || field?.trim() === ""
+    )
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const videoLocalPath = req.files?.video[0]?.path;
+
+  if (!videoLocalPath) throw new ApiError(401, "Video is required to publish");
+
+  const thumbnailLocalPath = req.files?.thumbnail[0]?.path;
+
+  if (!thumbnailLocalPath)
+    throw new ApiError(401, "Thumbnail is required to publish");
+
+  const [videoFile, thumbnailFile] = await Promise.all([
+    uploadOnCloudinary(videoLocalPath, video_upOptions),
+    uploadOnCloudinary(thumbnailLocalPath, thumbnail_upOptions),
+  ]);
+
+  if (!videoFile || !thumbnailFile) {
+    let errorMessage = "";
+    if (!videoFile) errorMessage += "Failed to upload video. ";
+    if (!thumbnailFile) errorMessage += "Failed to upload thumbnail.";
+    throw new ApiError(500, errorMessage);
+  }
+
+  const video = await Video.create({
+    video: {
+      fileId: videoFile.public_id,
+      url: videoFile.playback_url,
+    },
+    thumbnail: {
+      fileId: thumbnailFile.public_id,
+      url: thumbnailFile.secure_url,
+    },
+    duration: videoFile.duration,
+    title,
+    description,
+    isPublished,
+    owner: req.user._id,
+  });
+
+  if (!video) throw new ApiError(500, "Failed to publish video");
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, video, "Video published successfully"));
+});
 
 const getVideoByIdForGuest = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
